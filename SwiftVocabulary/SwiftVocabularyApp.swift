685056653374c947c4f61acba0e443a8d78ef187
Marc <marcjacques@me.com>
2022-03-29 21:22:26 -0400

    Initial Commit

---
+++ SwiftVocabulary/SwiftVocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftVocabularyApp.swift
+//  SwiftVocabulary
+//
+//  Created by Marc Jacques on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftVocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
